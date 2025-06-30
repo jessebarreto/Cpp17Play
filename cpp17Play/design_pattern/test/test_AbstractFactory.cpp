55d073f7897cd56cf78e69301f821ff581b63bb2
jessebarreto <jesseh.barreto@gmail.com>
2025-06-30 19:55:08 -0300

    Add README, LICENSE, AbstractFactory & Singleton

---
+++ cpp17Play/design_pattern/test/test_AbstractFactory.cpp
@@ -0,0 +1,131 @@
+#include <gtest/gtest.h>
+
+#include "../AbstractFactory.hpp"
+
+using namespace cpp17Play;
+
+namespace {
+
+class AbstractGeometry {
+protected:
+    AbstractGeometry(){};
+
+public:    
+    virtual ~AbstractGeometry(){};
+};
+
+class AbstractShape : public AbstractGeometry {
+protected:
+    AbstractShape(){};
+
+public:
+    virtual float area() const = 0;
+    
+    virtual ~AbstractShape(){};
+};
+
+
+class Rectangle : public AbstractShape {
+    float _width{0.0};
+    float _height{0.0};
+
+public:
+    Rectangle(float width = 0.0, float height = 0.0) : _width(width), _height(height) {}
+
+    float area() const override {
+        return _width * _height;
+    }
+};
+
+class Circle : public AbstractShape {
+    float _radius{0.0};
+    static constexpr float _PI{3.14159};
+
+public:
+    explicit Circle(float radius = 0.0) : _radius(radius) {}
+
+    float area() const override {
+        return _PI * _radius * _radius;
+    }
+};
+
+class AbstractSolid : public AbstractGeometry {
+protected:
+AbstractSolid(){};
+
+public:
+    virtual float volume() const = 0;
+    
+    virtual ~AbstractSolid(){};
+};
+    
+class Cuboid : public AbstractSolid {
+    Rectangle _base;
+    float _height{0.0};
+
+public:
+    Cuboid(const Rectangle& base, float height) : _base(base), _height(height) {}
+
+    float volume() const override {
+        return _base.area() * _height;
+    }
+};
+
+class Sphere : public AbstractSolid {
+    float _radius{0.0};
+    static constexpr float _PI{3.14159};
+
+public:
+    explicit Sphere(float radius) : _radius{radius} {}
+
+    float volume() const override {
+        return _radius*_radius*_radius*_PI*4/3;
+    }
+};
+
+enum ShapeEnum {
+    CircleT,
+    RectangleT,
+};
+
+class GeometryFactory : public AbstractFactory<AbstractGeometry, ShapeEnum> {
+public:
+    std::unique_ptr<AbstractGeometry> create(ShapeEnum enumerate) const {
+        if (enumerate == ShapeEnum::CircleT) {
+            return std::make_unique<Sphere>(1.0);
+        }
+        return std::make_unique<Cuboid>(Rectangle(1.0, 1.0), 1.0);
+    }
+};
+
+class ShapeFactory : public AbstractFactory<AbstractGeometry, ShapeEnum> {
+public:
+    std::unique_ptr<AbstractGeometry> create(ShapeEnum enumerate) const {
+        if (enumerate == ShapeEnum::CircleT) {
+            return std::make_unique<Circle>(1.0);
+        }
+        return std::make_unique<Rectangle>(1.0, 1.0);
+    }
+};
+
+TEST(AbstractFactoryTests, GIVEN_Inheriting_AbstractFactory_For_Shape_WHEN_Calling_Create_Method_THEN_Corretly_Apply_Factory) {
+    std::unique_ptr<AbstractFactory<AbstractGeometry, ShapeEnum>> factory = nullptr;
+
+    factory = std::make_unique<ShapeFactory>();
+    auto shape = factory->create(ShapeEnum::CircleT);
+
+    EXPECT_TRUE(dynamic_cast<Circle*>(shape.get()));
+}
+
+TEST(AbstractFactoryTests, GIVEN_Inheriting_AbstractFactory_For_Solid_WHEN_Calling_Create_Method_THEN_Corretly_Apply_Factory) {
+    std::unique_ptr<AbstractFactory<AbstractGeometry, ShapeEnum>> factory = nullptr;
+
+    factory = std::make_unique<GeometryFactory>();
+    auto solid = factory->create(ShapeEnum::CircleT);
+
+    EXPECT_TRUE(dynamic_cast<Sphere*>(solid.get()));
+}
+
+} // namespace
+
+
---
+++ cpp17Play/design_pattern/test/test_Singleton.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
+
+#include "../Singleton.hpp"
+
+using namespace cpp17Play;
+
+namespace {
+
+class Sphere {
+    float _radius{0.0};
+    static constexpr float _PI{3.14159};
+
+public:
+    explicit Sphere(float radius = 1.0) : _radius{radius} {}
+
+    void setRadius(float radius) {
+        _radius = radius;
+    }
+
+    float volume() const {
+        return _radius*_radius*_radius*_PI*4/3;
+    }
+
+    float radius() const noexcept {
+        return _radius;
+    }
+};
+
+void threadFooA() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(150));
+    auto& singleton = Singleton<Sphere>::instance();
+    singleton.setRadius(3);
+    EXPECT_FLOAT_EQ(singleton.radius(), 3.0);
+}
+
+void threadFooB() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    auto& singleton = Singleton<Sphere>::instance();
+    singleton.setRadius(2);
+    EXPECT_FLOAT_EQ(singleton.radius(), 2.0);
+}
+
+TEST(SingletonTests, Given__When__Then_) {
+    auto single_sphere = Singleton<Sphere>::instance(2.5);
+    // single_sphere.setRadius(2.5);
+    // auto single_sphere = Singleton<Sphere>::instance();
+
+    EXPECT_FLOAT_EQ(single_sphere.radius(), 2.5);
+    // EXPECT_EQ(1, 0);
+}
+
+TEST(SingletonTests, Given_Threads_When__Then_) {
+    std::thread threadA(threadFooA);
+    std::thread threadB(threadFooB);
+
+    threadA.join();
+    threadB.join();
+}
+
+} // namespace
+
+
