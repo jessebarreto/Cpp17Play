03673cff0642fc38bd9bcfb7c65142bf64b7ae74
jessebarreto <jesseh.barreto@gmail.com>
2025-07-02 21:16:33 -0300

    Run CLangFormat script

--- cpp17Play/design_pattern/structural_patterns/test/test_Adapter.cpp
+++ cpp17Play/design_pattern/structural_patterns/test/test_Adapter.cpp
@@ -2,39 +2,36 @@
 
 #include "../Adapter.hpp"
 
-
 using namespace cpp17Play;
 
 class Circle {
-    double _radius;
+  double _radius;
 
-public:
-    explicit Circle(double radius) : _radius(radius) {}
+ public:
+  explicit Circle(double radius) : _radius(radius) {}
 
-    double radius() const { return _radius; }
+  double radius() const { return _radius; }
 };
 
 class Square {
-    double _side;
+  double _side;
 
-public:
-    explicit Square(double side) : _side(side) {}
+ public:
+  explicit Square(double side) : _side(side) {}
 
-    double side() const { return _side; }
+  double side() const { return _side; }
 };
 
 class CircleSquareAdapter : public Adapter<Circle, Square> {
-public:
-    Square adapt(Circle& circle) const {
-        return Square(2 * circle.radius());
-    }
+ public:
+  Square adapt(Circle& circle) const { return Square(2 * circle.radius()); }
 };
 
 TEST(AdapterTest, WHEN_Adapting_THEN_SuccessfullyAdapts) {
-    Circle circle(1.0);
+  Circle circle(1.0);
 
-    CircleSquareAdapter adapter;
-    auto square = adapter.adapt(circle);
+  CircleSquareAdapter adapter;
+  auto square = adapter.adapt(circle);
 
-    EXPECT_EQ(square.side(), 2.0);
+  EXPECT_EQ(square.side(), 2.0);
 }
